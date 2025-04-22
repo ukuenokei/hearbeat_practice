67507034831c4cd6829bdf0e367ecdccc68af4ba
Kei Ueno <kei-ueno@nec.com>
2025-04-22 03:37:00 -0400

    受信メッセージ比較による処理を追加

--- client.c
+++ client.c
@@ -11,82 +11,95 @@
 #define PORT 8000
 #define ECHOSTRING "ImAlive"
 #define BUFSIZE 16
-#define MONITOR_MAX 1000000 /*HBをやり取りする最大値*/
 #define TIMEOUT 1
 #define INTERVAL 3
-#define RETRY_MAX 5
+#define RETRY_MAX 3
 
 #define ALIVE 0
 #define DEAD 1
 
 struct nodemap {
-  /*Alive:0, Down 1*/
-  int self;
-  int peer;
+    int self;
+    int peer;
 };
 
 int main() {
-  int sock;
-  struct sockaddr_in serv_addr;
-  int serv_addr_len;
-  char message[BUFSIZE] = ECHOSTRING;
-  char buffer[BUFSIZE];
-  struct timeval timeout;
-  struct nodemap nm;
+    int sock;
+    struct sockaddr_in serv_addr;
+    int serv_addr_len;
+    char message[BUFSIZE] = ECHOSTRING;
+    char buffer[BUFSIZE];
+    struct timeval timeout;
+    struct nodemap nm;
 
-  timeout.tv_sec = TIMEOUT;
-  timeout.tv_usec = 0;
-  nm.self = ALIVE;
-  nm.peer = ALIVE;
-  if (&serv_addr == NULL) {
-    perror("serv_addr is NULL");
-    exit(EXIT_FAILURE);
-  }
-  memset(&serv_addr, 0, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = inet_addr(PEERIP);
-  serv_addr.sin_port = htons(PORT);
+    // timeout.tv_sec = 0;
+    // timeout.tv_usec = 100 * 1000; /*100msec*/
+    timeout.tv_sec = TIMEOUT;
+    timeout.tv_usec = 0;
+    nm.self = ALIVE;
+    nm.peer = ALIVE;
 
-  if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
-    perror("socket() failed");
-    exit(EXIT_FAILURE);
-  }
-
-  if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) <
-      0) {
-    perror("setsockopt(RCVTIMEO) failed");
-    exit(EXIT_FAILURE);
-  }
-
-  for (int i = 0; i < MONITOR_MAX; i++) {
-    if (nm.peer == DEAD) break;
-    if (sendto(sock, message, sizeof(message), 0, (struct sockaddr *)&serv_addr,
-               sizeof(serv_addr)) < 0) {
-      perror("sendto() failed");
-    }
-
-    serv_addr_len = sizeof(serv_addr);
-
-    /**********************************サーバーからの確認処理***********************************/
-    if (recvfrom(sock, buffer, BUFSIZE, 0, (struct sockaddr *)&serv_addr,
-                 &serv_addr_len) < 0) {
-      /*タイムアウト時のみ継続、そうでないなら落とす*/
-      if (errno == EWOULDBLOCK) {
-        printf("recvfrom() timeout retry\n");
-        nm.peer = DEAD;
-        break;
-      } else {
-        perror("recvfrom() failed");
+    if (&serv_addr == NULL) {
+        perror("serv_addr is NULL");
         exit(EXIT_FAILURE);
-      }
     }
-    printf("Peer server is alive\n");
-    sleep(INTERVAL);
-  }
-  /*****************************************************************************************/
-  if (close(sock) < 0) {
-    perror("close() failed");
-    exit(EXIT_FAILURE);
-  }
-  return 0;
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(PEERIP);
+    serv_addr.sin_port = htons(PORT);
+
+    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+        perror("socket() failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) <
+        0) {
+        perror("setsockopt(RCVTIMEO) failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int i = 0;
+    while (1) {
+        if (sendto(sock, message, sizeof(message), 0,
+                   (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+            perror("sendto() failed");
+        }
+        /**********************************サーバーからの確認処理***********************************/
+        serv_addr_len = sizeof(serv_addr);
+        if (recvfrom(sock, buffer, BUFSIZE, 0, (struct sockaddr *)&serv_addr,
+                     &serv_addr_len) < 0) {
+            /*recvfromが失敗したとき*/
+            if (errno == EWOULDBLOCK) {
+                /*タイムアウト時のみ継続*/
+                printf("recvfrom() timeout\n");
+                i++; /*タイムアウトカウント*/
+                if (i == RETRY_MAX) {
+                    nm.peer = DEAD;
+                }
+                if (nm.peer == DEAD) {
+                    printf("Peer server is dead\n");
+                    break;
+                }
+                continue;
+            } else {
+                /*それ以外は終了*/
+                perror("recvfrom() failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+        if (strcmp(buffer, message) != 0) {
+            /*期待したメッセージを受信しなかった場合*/
+            printf("Message Error : %s\n",buffer);
+            break;
+        }
+        /*期待したメッセージを受信したらここに来る
+        recvfrom()> 0 -> strcmp()==0*/
+        printf("Peer server is alive\n");
+        nm.peer = ALIVE;
+        sleep(INTERVAL);
+        /*****************************************************************************************/
+    }
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,16 +1,15 @@
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
+#include <unistd.h>
 
 #define PORT 8000
 #define ECHOSTRING "ImAlive"
 #define BUFSIZE 16
-#define MONITOR_MAX 1000000
-int main()
-{
+
+int main() {
     int sock;
     struct sockaddr_in serv_addr;
     struct sockaddr_in client_addr;
@@ -19,8 +18,7 @@ int main()
     char message[BUFSIZE] = ECHOSTRING;
     char buffer[BUFSIZE];
 
-    if (&serv_addr == NULL)
-    {
+    if (&serv_addr == NULL) {
         perror("serv_addr is NULL");
         exit(EXIT_FAILURE);
     }
@@ -29,40 +27,29 @@ int main()
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(PORT);
 
-    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
-    {
+    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
         perror("socket() failed");
         exit(EXIT_FAILURE);
     }
 
-    if (bind(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)))
-    {
+    if (bind(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) {
         perror("bind() failed");
         exit(EXIT_FAILURE);
     }
-    for (int i = 0; i < MONITOR_MAX; i++)
-    {
+    while (1) {
         client_addr_len = sizeof(client_addr);
         if (recvfrom(sock, buffer, sizeof(buffer), 0,
-                     (struct sockaddr *)&client_addr, &client_addr_len) < 0)
-        {
+                     (struct sockaddr *)&client_addr, &client_addr_len) < 0) {
             perror("recvfrom() failed");
             exit(EXIT_FAILURE);
-        }
-        else{
-            printf("Hndling client : %s\n",inet_ntoa(client_addr.sin_addr));
+        } else {
+            printf("Hndling client : %s\n", inet_ntoa(client_addr.sin_addr));
         }
         if (sendto(sock, message, sizeof(message), 0,
-                   (struct sockaddr *)&client_addr, client_addr_len) < 0)
-        {
+                   (struct sockaddr *)&client_addr, client_addr_len) < 0) {
             perror("sendto() failed");
             exit(EXIT_FAILURE);
         }
-    }
-    if (close(sock) < 0)
-    {
-        perror("close() failed");
-        exit(EXIT_FAILURE);
     }
 
     return 0;
